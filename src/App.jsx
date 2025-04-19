6c336bc8d4c52da8e81df26caaadf604e2bab406
ruslantur77 <ruslantursunov77@gmail.com>
2025-04-19 10:53:14 +0300

    beautify

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,15 @@ import Header from './components/Header.jsx'
 import './components/TextArea.jsx'
 import Footer from './components/Footer.jsx'
 import TextAreaWithButton from './components/TextAreaWithButton.jsx'
+
 function App() {
   return (
     <div>
     <Header />
-
     <div className='paragraph'>
     <h2>Summary AI кратко перескажет видео c YouTube, а также даст список самых повторящихся слов</h2> 
       <TextAreaWithButton />
     </div>
-
     <Footer/>
     </div>
   )
