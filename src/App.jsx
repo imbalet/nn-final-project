dd5f75d27387e5375d877f2e704fefb90d82224a
ruslantur77 <ruslantursunov77@gmail.com>
2025-04-19 11:22:15 +0300

    Added route

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
 import { useState } from 'react'
+import { Routes, Route } from 'react-router-dom'
 import './App.css'
 import './components/Header.jsx'
 import Header from './components/Header.jsx'
 import './components/TextArea.jsx'
 import Footer from './components/Footer.jsx'
 import TextAreaWithButton from './components/TextAreaWithButton.jsx'
+import Summary from './pages/Summary.jsx'
 
 function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home/>} />
+      <Route path="/summary" element={<Summary />} />
+    </Routes>
+  )
+}
+
+function Home(){
   return (
     <div>
     <Header />
---
+++ src/pages/Summary.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import './Summary.css'
+import '../components/Header.jsx'
+import Header from '../components/Header.jsx'
+import '../components/TextArea.jsx'
+import Footer from '../components/Footer.jsx'
+import TextAreaWithButton from '../components/TextAreaWithButton.jsx'
+
+function Summary() {
+  return (
+    <div>
+    <Header />
+    <div className='paragraph'>
+    <h2>Тут будет Суммаризация</h2> 
+      <TextAreaWithButton />
+    </div>
+    <Footer/>
+    </div>
+  )
+}
+
+export default Summary
