93793cb7c8eecfef1fbfa13906bd5b3eba7c8220
ruslantur77 <ruslantursunov77@gmail.com>
2025-04-21 20:18:47 +0300

    added some features to Header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,13 +1,29 @@
-import './Header.css'
-import logo from '/src/assets/logo.svg'
+import './Header.css';
+import logo from '/src/assets/logo.svg';
+import { useNavigate } from 'react-router-dom';
 
-export default function Header(){
-    return(
+export default function Header() {
+    const navigate = useNavigate();
+
+    const handleLogoClick = () => {
+        navigate('/'); 
+    };
+
+    return (
         <header className="header">
-            <h1>Summary AI</h1>
-            <div className="logo" >
-            <img src={logo}/>
+            <h1 
+                className="clickable-title" 
+                onClick={handleLogoClick}
+                style={{ cursor: 'pointer' }}
+            >
+                Summary AI
+            </h1>
+            <div className="logo">
+                <img 
+                    src={logo} 
+                    alt="Logo" 
+                />
             </div>
         </header>
-    )
+    );
 }
